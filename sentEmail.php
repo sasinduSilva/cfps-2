46508736e950f615ec2a548044c45ab7ec1f489d
sasinduSilva <sinharaavishka@gmail.com>
2025-08-28 17:06:41 +0530

    mailer installed

--- sentEmail.php
+++ sentEmail.php
@@ -5,6 +5,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $subject = strip_tags(trim($_POST["subject"]));
     $message = trim($_POST["message"]);
+
+    require "vendor/autoload.php";
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+
+    $mail = new PHPMailer(true);
+
+    $sentFrom = "info@cfpslawschool.com";
+
+    $mail->isSMTP();
+    $mail->SMPTPAuth = true;
+    $mail->Host = "mail.thelanka.net"
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+    $mail->Username = "riyard@thelanka.net"
+    $mail->Password = "12nV10FILQMF"
+    $mail->setFrom($email,$name);
+    $mail->addAddress("info@cfpslawschool.com","Test")
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+    $mail->send();
+
+    echo "email sent";
+
     
     // Check that data was sent to the script
     if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -12,27 +37,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Please complete the form and try again.";
         exit;
     }
+
+
     
-    // Set the recipient email address
-    $recipient = "sinharaavishka@gmail.com";
     
-    // Build the email content
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Subject: $subject\n\n";
-    $email_content .= "Message:\n$message\n";
-    
-    // Build the email headers
-    $email_headers = "From: $name <$email>";
-    
-    // Send the email
-    if (mail($recipient, $subject, $email_content, $email_headers)) {
-        http_response_code(200);
-        echo "Thank You! Your message has been sent.";
-    } else {
-        http_response_code(500);
-        echo "Oops! Something went wrong and we couldn't send your message.";
-    }
 } else {
     http_response_code(403);
     echo "There was a problem with your submission, please try again.";
